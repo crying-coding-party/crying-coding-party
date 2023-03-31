73d5ae36246591fbd908c67c263ad643f2452bf5
juhyojeong <juhyojeong58@gmail.com>
2023-03-31 19:26:21 +0900

    1780.js

--- 재귀/jhj2713/1780.js
+++ 재귀/jhj2713/1780.js
@@ -1,22 +1,38 @@
-const input = `9
-0 0 0 1 1 1 -1 -1 -1
-0 0 0 1 1 1 -1 -1 -1
-0 0 0 1 1 1 -1 -1 -1
-1 1 1 0 0 0 0 0 0
-1 1 1 0 0 0 0 0 0
-1 1 1 0 0 0 0 0 0
-0 1 -1 0 1 -1 0 1 -1
-0 -1 1 0 1 -1 0 1 -1
-0 1 -1 1 0 -1 0 1 -1`.split("\n");
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 const n = parseInt(input[0]);
 const integers = input.slice(1).map((inp) => inp.split(" ").map(Number));
 const answer = [0, 0, 0];
 
-function recur(count) {
-  for (let i = 0; i < n / count; i++) {
-    for (let j = 0; j < n / count; j++) {
-      const tmp = integers[i * (n / count)][j * (n / count)];
+function recur(count, x, y) {
+  if (count === 1) {
+    answer[integers[x][y] + 1] += 1;
+    return;
+  }
+
+  const tmp = integers[x][y];
+  let flag = false;
+  for (let i = 0; i < count; i++) {
+    for (let j = 0; j < count; j++) {
+      if (integers[x + i][y + j] !== tmp) {
+        flag = true;
+        break;
+      }
+    }
+    if (flag) break;
+  }
+
+  if (!flag) answer[tmp + 1] += 1;
+  else {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        recur(count / 3, x + i * (count / 3), y + j * (count / 3));
+      }
     }
   }
 }
+
+recur(n, 0, 0);
+
+console.log(answer.join("\n"));
