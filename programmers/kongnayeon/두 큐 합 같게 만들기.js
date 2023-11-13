c56e871a7d72f9da7f699953fdf75c7d7c3a0ab3
강나연 <seoultownroad@naver.com>
2023-11-13 17:42:52 +0900

    프로그래머스: 두 큐 합 같게 만들기 - 시간 초과3

--- programmers/kongnayeon/두 큐 합 같게 만들기.js
+++ programmers/kongnayeon/두 큐 합 같게 만들기.js
@@ -2,12 +2,10 @@ function solution(queue1, queue2) {
   let answer = 0;
 
   let sum = 0;
-  queue1.forEach((num) => {
-    sum += num;
-  });
-  queue2.forEach((num) => {
-    sum += num;
-  });
+  let sum1 = queue1.reduce((acc, num) => acc + num, 0);
+  let sum2 = queue2.reduce((acc, num) => acc + num, sum);
+  sum = sum1 + sum2;
+
   sum = sum / 2;
 
   let max = 2 * queue1.length + queue2.length - 2;
@@ -18,17 +16,21 @@ function solution(queue1, queue2) {
   while (answer != max) {
     let el = 0;
 
-    let sum1 = queue1.reduce((acc, num) => acc + num, 0);
-    let sum2 = queue2.reduce((acc, num) => acc + num, 0);
+    // let sum1 = queue1.reduce((acc, num) => acc + num, 0);
+    // let sum2 = queue2.reduce((acc, num) => acc + num, 0);
 
     if (sum1 === sum2) {
       return answer;
     } else if (sum1 > sum2) {
       el = queue1.shift();
       queue2.push(el);
+      sum1 -= el;
+      sum2 += el;
     } else {
       el = queue2.shift();
       queue1.push(el);
+      sum2 -= el;
+      sum1 += el;
     }
     answer++;
   }
