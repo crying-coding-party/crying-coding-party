aa9a2bad7e24b6459172d175485fd39384e18ab7
chaaerim <cofla411@ewhain.net>
2024-01-21 21:36:15 +0900

    백트래킹: 후보키.js

--- programmers/chaaerim/후보키.js
+++ programmers/chaaerim/후보키.js
@@ -1,5 +1,3 @@
-let ans = [];
-
 solution([
   ['100', 'ryan', 'music', '2'],
   ['200', 'apeach', 'math', '2'],
@@ -10,34 +8,50 @@ solution([
 ]);
 
 function solution(relation) {
-  //   relation[0].forEach((_, idx) => (ans[`${idx + 1}`] = []));
-  for (let i = 0; i < relation.length; i++) {
+  let ans = [];
+
+  relation.forEach(el => el.forEach((_, idx) => {}));
+  for (let i = 0; i < 1; i++) {
     for (let j = 0; j < relation[0].length; j++) {
       const array = new Array(j + 1).fill(0);
       const visited = new Array(relation[0].length).fill(false);
-      back(relation[i], j + 1, 0, 0, array, visited);
+      back(relation[i], j + 1, 0, 0, array, visited, ans);
     }
   }
-  console.log(ans);
-  return;
+  //   console.log(ans);
+  let count = 0;
+  let num = 0;
+  const skip = [];
+  while (count < ans.length) {
+    const col = ans[count].split('');
+    if (skip.filter(item => item.every(v => col.includes(v))).length !== 0) {
+      count++;
+      continue;
+    }
+    const key = relation.flatMap(val => col.map(item => val[item]).join(''));
+    if ([...new Set(key)].length === relation.length) {
+      skip.push(col);
+      num++;
+    }
+    count++;
+  }
+  console.log(num);
+  return num;
 }
 
-function back(list, endNum, n, start, array, visited) {
+function back(list, endNum, n, start, array, visited, ans) {
   //   console.log(list, endNum, array, visited);
 
   if (endNum === n) {
-    console.log(array, endNum, start);
-    // ans[`${endNum}`].push(array.join(' '));
-    // console.log(ans);
-    ans.push(array.join(' '));
+    ans.push(array.join(''));
     return;
   }
 
   for (let i = start; i < list.length; i++) {
     if (!visited[i]) {
-      array[n] = list[i];
+      array[n] = i;
       visited[i] = true;
-      back(list, endNum, n + 1, i + 1, array, visited);
+      back(list, endNum, n + 1, i + 1, array, visited, ans);
       visited[i] = false;
     }
   }
